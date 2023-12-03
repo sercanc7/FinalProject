8ba898d88be076db997b02d74cd94ce34c5aa67b
sercanc7 <80421784+sercanc7@users.noreply.github.com>
2023-12-03 04:49:20 +0300

    Day 11

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,63 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController] // => Attribute
+    public class ProductsController : ControllerBase
+    {
+        // Loosely coupled
+        //IoC Container => Inversion of control
+        IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //Dependency chain
+
+            var result = _productService.GetAll();
+            if (result.Success)
+            {   //200 => Ok Request
+                return Ok(result);
+            }
+            // 400 => Bad Request
+            return BadRequest(result);
+        }
+
+        [HttpGet("getbyid")] // => getbyid diyerek alias olarak belirledik.
+
+        public IActionResult GetById(int id) 
+        {
+            var result = _productService.GetById(id);
+            if(result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result);
+        
+        }
+
+        [HttpPost("add")]
+
+        public IActionResult Add(Product product)
+        {
+            var result = _productService.Add(product);
+            if(result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+    }
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,42 @@
+
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+
+namespace WebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();          
+            builder.Services.AddSingleton<IProductService, ProductManager>();
+            builder.Services.AddSingleton<IProductDal, EfProductDal>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
